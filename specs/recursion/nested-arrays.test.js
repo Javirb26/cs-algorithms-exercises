94eec8cc868ae0991e5e4636a13ebb8c53818a35
Javier Ballester <Javirb26@gmail.com>
2024-02-14 17:35:57 -0500

    solved for nested-array recursion

--- specs/recursion/nested-arrays.test.js
+++ specs/recursion/nested-arrays.test.js
@@ -6,16 +6,50 @@
 
   example: nestedAdd([1, 2, [3]]) = 6
            nestedAdd([[[2]], 1, [1, 3]]) = 7
+
+
  
  */
+// Iterative Approach
+// function nestedAdd(array) {
+//   // write code here
+//   let flattened = array.flat(Infinity);
+//   let sum = 0;
+//   flattened.forEach(element => {
+//     if (typeof element == 'number'){
+//       sum += element
+//     }
+//   })
+//   console.log(sum)
+// }
+// nestedAdd([1, 2, 3])
 
 function nestedAdd(array) {
-  // write code here
+  let sum = 0;
+  for(let i = 0; i < array.length; i++){
+    const current = array[i];
+    if (Array.isArray(current)){
+      sum += nestedAdd(current);
+    } else {
+      sum += current;
+    }
+  }
+  return sum;
 }
 
-test.skip("nested arrays addition", () => {
-  expect(nestedAdd([1, 2, 3])).toEqual(6);
-  expect(nestedAdd([1, [2], 3])).toEqual(6);
-  expect(nestedAdd([[[[[[[[[5]]]]]]]]])).toEqual(5);
-  expect(nestedAdd([10, [12, 14, [1], [16, [20]]], 10, 11])).toEqual(94);
-});
+if (nestedAdd([1, [2], 3]) &&
+    nestedAdd([1, 2, 3]) == 6 &&
+    nestedAdd([[[[[[[[[5]]]]]]]]]) &&
+    nestedAdd([10, [12, 14, [1], [16, [20]]], 10, 11]) == 94)
+{
+  console.log("All tests passed!");
+} else {
+  console.log("Did not Pass.");
+}
+
+console.log(nestedAdd([1, [2], 3]))
+console.log(nestedAdd([1, 2, 3]) == 6)
+console.log(nestedAdd([1, [2], 3]) == 6)
+console.log(nestedAdd([[[[[[[[[5]]]]]]]]]) == 5)
+console.log(nestedAdd([10, [12, 14, [1], [16, [20]]], 10, 11]) == 94)
+
